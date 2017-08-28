d329e50270a698be621b1527168fca9e11b9916d
antonzhelyazkov <antonisimo@gmail.com>
2017-08-28 16:30:02 +0300

    beta
    1. Add restore script
    2. Full backup

--- zimbraBackup.sh
+++ zimbraBackup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ftpHost=10.10.10.10
-ftpUser=test    
+ftpUser=test1
 ftpPass=qweqweqwe
 
 verbose=1
@@ -38,10 +38,12 @@ userpassDir="$currentBackupDir/userpass"
 userdataDir="$currentBackupDir/userdata"
 filtersDir="$currentBackupDir/filters"
 mailDir="$currentBackupDir/mail"
+restoreScriptsDir="$currentBackupDir/restoreScripts"
 domainsFile="$currentBackupDir/domains.txt"
 adminsFile="$currentBackupDir/admins.txt"
 emailsFile="$currentBackupDir/emails.txt"
 distributinlistFile="$currentBackupDir/distributinlist.txt"
+restoreCreateUsers="$restoreScriptsDir/createUsers.sh"
 
 ##############################
 
@@ -118,6 +120,12 @@ else
         echo $$ > $nagiosLog
 fi
 
+if [[ $EUID -ne 0 ]]; then
+        logPrint "This script must be run as root. Current user $EUID" 1 1
+else
+        logPrint "Current UID is $EUID" 0 0
+fi
+
 if [ ! -d $localBackupDir ]; then
         logPrint "Directory $localBackupDir is does NOT exist" 0 1
 else
@@ -161,6 +169,7 @@ createDir $userpassDir
 createDir $userdataDir
 createDir $filtersDir
 createDir $mailDir
+createDir $restoreScriptsDir
 
 logPrint "create distributinlists file $domainsFile" 0 0
 $zmprovCommand gad > $domainsFile
@@ -220,15 +229,14 @@ do
         fi
 done < $emailsFile
 
-#while read email
-#do
-#       logPrint "get filter for $email" 0 0
-#       $zmprovCommand ga $email zimbraMailSieveScript > $tmpDir/$email
-#       sed -i -e "1d" $tmpDir/$email
-#       sed 's/zimbraMailSieveScript: //g' $tmpDir/$email > $filtersDir/$email
-#done < $emailsFile
+while read email
+do
+        logPrint "get filter for $email" 0 0
+        $zmprovCommand ga $email zimbraMailSieveScript > $tmpDir/$email
+        sed -i -e "1d" $tmpDir/$email
+        sed 's/zimbraMailSieveScript: //g' $tmpDir/$email > $filtersDir/$email
+done < $emailsFile
 
-#for email in `cat /backups/zmigrate/emails.txt`; do  zmmailbox -z -m $i getRestURL '/?fmt=tgz' > $i.tgz ;  echo $email ; done
 while read email
 do
         logPrint "get mail for $email" 0 0
@@ -238,6 +246,144 @@ do
                 logPrint "ERROR could not execute $zmmailboxCommand -z -m $email getRestURL '/?fmt=tgz' > $mailDir/$email.tgz" 1 0
         fi
 done < $emailsFile
+
+############# restore scripts ##############
+
+cat <<END >$restoreCreateUsers
+#!/bin/bash
+
+currentDir=\$(dirname \$0)
+currentBaseDir=\$(echo "\$currentDir" | sed -r 's/restoreScripts//g')
+echo \$currentBaseDir
+
+tmpDir="\$currentBaseDir/tmp"
+distributinlistMembersDir="\$currentBaseDir/distributinlistMembers"
+userpassDir="\$currentBaseDir/userpass"
+userdataDir="\$currentBaseDir/userdata"
+filtersDir="\$currentBaseDir/filters"
+mailDir="\$currentBaseDir/mail"
+
+zimbraUser="zimbra"
+zmprovBin="/opt/zimbra/bin/zmprov"
+zmprovCommnand="sudo -u \$zimbraUser \$zmprovBin"
+
+zmmailboxBin="/opt/zimbra/bin/zmmailbox"
+zmmailboxCommand="sudo -u \$zimbraUser \$zmmailboxBin"
+
+##############################################
+
+function usage() {
+
+echo "Usage: ./createUser.sh --restore=all"
+echo "Usage: ./createUser.sh --restore=test@exampe.com"
+echo "--restore         >>>>    restore email or all emails"
+exit 1
+
+}
+
+##############################################
+
+for i in "\$@"
+do
+case \$i in
+        --restore=*)
+        restore="\${i#*=}"
+        shift # past argument=value
+        ;;
+
+        *)
+        # unknown option
+        ;;
+esac
+done
+
+if [[ -z \$restore ]]; then
+        echo "ERROR: restore parameter is not set"
+        usage
+fi
+
+emailRegex="^[a-z0-9!#\\$%&'*+/=?^_\\\`{|}~-]+(\.[a-z0-9!#$%&'*+/=?^_\\\`{|}~-]+)*@([a-z0-9]([a-z0-9-]*[a-z0-9])?\.)+[a-z0-9]([a-z0-9-]*[a-z0-9])?\\$"
+echo \$restore
+
+if [[ \$restore =~ \$emailRegex ]] || [ \$restore == "all" ]; then
+        echo "restore seems valid input \$restore"
+else
+        echo "restore seems NOT valid input \$restore"
+        usage
+fi
+
+if [[ \$EUID -ne 0 ]]; then
+        echo "This script must be run as root. Current user \$EUID"
+        exit
+else
+        echo "Current UID is \$EUID"
+fi
+
+if [ ! -d \$distributinlistMembersDir ] || [ ! -d \$userpassDir ] || [ ! -d \$userdataDir ] || [ ! -d \$filtersDir ] || [ ! -d \$mailDir ]; then
+        echo "some directories are missing."
+        echo "check \$distributinlistMembersDir"
+        echo "check \$userpassDir"
+        echo "check \$userdataDir"
+        echo "check \$filtersDir"
+        echo "check \$mailDir"
+        exit
+fi
+
+if [ \$restore == "all" ]; then
+        users=\$(<\$currentBaseDir/emails.txt)
+else
+        users=\$restore
+        grep -qw \$users \$currentBaseDir/emails.txt
+        checkIfEmail=\$?
+        if [ \$checkIfEmail -ne 0 ]; then
+                echo "email \$users does not exist in archive - \$currentBaseDir/emails.txt"
+                exit
+        fi
+fi
+
+for email in \$users
+do
+        \$zmprovCommnand -l gaa | grep -q \$email
+        checkIfAccountExists=\$?
+        if [[ \$checkIfAccountExists -eq 0 ]]; then
+                echo "Account \$email alredy exists."
+        else
+                echo "Account \$email does NOT exit"
+                givenName=\$(grep givenName: \$userdataDir/\$email | cut -d ":" -f2 | sed 's/ //g')
+                displayName=\$(grep displayName: \$userdataDir/\$email | cut -d ":" -f2 | sed 's/ //g')
+                shadowpass=\$(cat \$userpassDir/\$email.shadow)
+                echo "create account \$email givenName \$givenName displayName \$displayName"
+                tmpPass="CHANGEme"
+                \$zmprovCommnand ca \$email CHANGEme cn "\$givenName" displayName "\$displayName" givenName "\$givenName"
+                checkCreateUser=\$?
+                if [ \$checkCreateUser -eq 0 ]; then
+                        echo "user \$email created"
+                else
+                        echo "ERROR could not create \$zmprovCommnand ca \$email CHANGEme cn \$givenName displayName \$displayName givenName \$givenName"
+                fi
+                \$zmprovCommnand ma \$email userPassword \$shadowpass
+                checkPasswordSet=\$?
+                if [ \$checkPasswordSet -eq 0 ]; then
+                        echo "password set on \$email"
+                else
+                        echo "ERROR could not set password for user \$email"
+                fi
+
+                echo "restore mail for \$email"
+                \$zmmailboxCommand -z -m \$email postRestURL "/?fmt=tgz&resolve=skip" \$mailDir/\$email.tgz
+                checkMailRestore=\$?
+                if [ \$checkPasswordSet -eq 0 ]; then
+                        echo "mail restored \$email"
+                else
+                        echo "ERROR could not restore mail for \$email"
+                fi
+        fi
+done
+END
+
+chmod 755 $restoreCreateUsers
+
+############# restore scripts ##############
 
 if [ $keepRemoteCopy -eq 1 ]; then
 
